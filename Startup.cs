2c3e8ae5a410ab5d03ef34bdc8b05e97c8d22033
NGK <59110984+nandu-g-krishnan@users.noreply.github.com>
2021-05-06 20:44:19 +0530

    chaptor 4 html attribue data annotations

--- Startup.cs
+++ Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -14,13 +15,24 @@ namespace explorecalifornia
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        private readonly IConfiguration configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
         public void ConfigureServices(IServiceCollection services)
         {
+           
+
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseExceptionHandler("/error.html");
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -28,13 +40,27 @@ namespace explorecalifornia
 
             app.UseRouting();
 
-            app.UseEndpoints(endpoints =>
+            app.Use(async (context, next) =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                if (context.Request.Path.Value.Contains("invalid"))
+                    throw new Exception("ERROR!");
+
+                await next();
             });
+
+            app.UseEndpoints(routes => {
+                routes.MapControllerRoute("Default",
+                    "{controller=Home}/{action=Index}/{id?}"
+                );
+            });
+            app.UseFileServer();
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapGet("/", async context =>
+            //    {
+            //        await context.Response.WriteAsync("Hello World!");
+            //    });
+            //});
         }
     }
 }
